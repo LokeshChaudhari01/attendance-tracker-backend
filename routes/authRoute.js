3b3cf1366a43f1459e46702d83cf328c7666d982
Lokesh Chaudhari <home_folder@Lokeshs-MacBook-Air-2.local>
2026-01-30 10:40:19 +0530

    final changes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -16,6 +16,6 @@ router.get("/", (req, res) => {
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/me", getMe);
+router.get("/me", getMe);
 
 module.exports = router;
